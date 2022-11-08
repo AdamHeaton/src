c99fa45e347b9f4c61249c96dbfea4698be2014f
Adam <d00262386@student.dkit.ie>
2022-11-08 12:59:01 +0000

    V2 Tuesday

--- raylib_game.c
+++ raylib_game.c
@@ -8,16 +8,32 @@
 *   Configuration: Default
 *   Build target: [raylib_game]
 *   Launch target [raylib_game]
-*
-*
 ********************************************************************************************/
 
 #include "raylib.h"
 
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
+int score = 0;
+int bounce = 0;
 
+typedef struct Ball
+{
+    int radius;
+    int speedX;
+    int speedY;
+    int x;
+    int y;
+};
+
+/*typedef struct Paddle
+{
+    float x, y;
+    float speed;
+    float width, height;
+    void Draw()
+    {
+
+    }
+    */
 
 //----------------------------------------------------------------------------------
 // Main entry point
@@ -31,12 +47,22 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Pong Game");
 
-    Vector2 ballPosition = {screenWidth/2, screenHeight/2};
-    Color ballColor = DARKBLUE;
+    struct Ball ball;
+    ball.radius = 10;
+    ball.x = screenWidth/2;
+    ball.y = screenHeight/2;
+    ball.speedX = 3;
+    ball.speedY = 5;
+
+    int borderWidth = 25;
+    int borderHeight= 25;
     int paddlePosition = screenHeight/2;
-    int paddleWidth = 10;
-    int paddleHeight = 50;
-    int ballRadius = 10;
+    int paddleWidth = 25;
+    int paddleHeight = 75;
+
+    
+    //struct Paddle leftPaddle;
+
 
     SetTargetFPS(60);                 // Set our game to run at 60 frames-per-second
     //---------------------------------------------------------------------------------------
@@ -55,6 +81,13 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
+            //BORDER CODE, do I want to implement or not?
+            DrawRectangle(0,0, borderWidth, screenHeight, LIGHTGRAY);
+            DrawRectangle(0,0, screenWidth, borderHeight, LIGHTGRAY);
+            DrawRectangle(0, screenHeight-borderHeight, screenWidth, screenHeight, LIGHTGRAY);
+            DrawRectangle(screenWidth-borderWidth, 0, borderWidth, screenHeight, LIGHTGRAY);
+
+
             DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, LIGHTGRAY);                
             // Draw a line across the middle of the screen vertically, for testing
             DrawLine(0, screenHeight/2, screenWidth, screenHeight/2, LIGHTGRAY);                
@@ -67,21 +100,61 @@ int main(void)
             if ((paddlePosition + paddleHeight/2) >= screenHeight) paddlePosition = screenHeight-paddleHeight/2;    
             /* and this does roughly the same in order to check that the paddle doesn't go off the bottom of the screen */
         
-            DrawRectangle((screenWidth/100*1)-paddleWidth/2, paddlePosition-paddleHeight/2, paddleWidth, paddleHeight, BLACK);
+            DrawRectangle(0, paddlePosition-paddleHeight/2, paddleWidth, paddleHeight, BLACK);
             /* This line of code draws our left paddle. It factors in the paddle's height and width in order to draw the paddle from 
             the centre instead of the top left.
             */
-            DrawRectangle((screenWidth/100*99)-paddleWidth/2, (screenHeight-paddleHeight)-(paddlePosition-paddleHeight/2), paddleWidth, paddleHeight, BLACK);
+            DrawRectangle(screenWidth-paddleWidth, (screenHeight-paddleHeight)-(paddlePosition-paddleHeight/2), paddleWidth, paddleHeight, BLACK);
             /* This line of code draws our right paddle. It factors in the paddle's height and width in order to draw the paddle from 
             the centre instead of the top left. */
 
-            DrawCircle((screenWidth/2), screenHeight/2, ballRadius, RED);
+            
+            //Ball bouncing on right hand side of screen being returned
+            if (ball.x >= screenWidth-borderWidth-ball.radius) {
+                if ((ball.y < screenHeight-(paddlePosition-paddleHeight/2)) && (ball.y > screenHeight-(paddlePosition+paddleHeight/2))) {
+                    DrawRectangle(screenWidth/2-50, screenHeight/2-50, 100, 100, RED);
+                    score++;
+                }
+                ball.speedX = ball.speedX*-1; //return the ball by reversing the ball's X speed
+                bounce++;
+            }
+            
+           //Ball bouncing on bottom section of screen being returned
+            if (ball.y >= screenHeight-borderHeight-ball.radius) {
+                ball.speedY = ball.speedY*-1; //return the ball by reversing the ball's Y speed
+                bounce++;
+            }
 
+            //ball bouncing on left hand side of screen being returned
+            if (ball.x <= ball.radius+borderWidth) {
+                if ((ball.y > paddlePosition-paddleHeight/2) && (ball.y < paddlePosition+paddleHeight/2)) {
+                    DrawRectangle(screenWidth/2-50, screenHeight/2-50, 100, 100, RED);
+                    score++;
+                }
+                ball.speedX = ball.speedX*-1; //return the ball by reversing the ball's X speed
+                bounce++;
+            }
+            
+            //ball bouncing on top section of screen being returned
+            if (ball.y <= ball.radius+borderWidth) {
+                ball.speedY = ball.speedY*-1; //return the ball by reversing the ball's Y speed
+                bounce++;
+            }
+            
+            
+            ball.x -= ball.speedX;
+            ball.y -= ball.speedY;
 
-
-
+            DrawCircle(ball.x, ball.y, ball.radius, RED);
 
             DrawText(TextFormat("Paddle position Y: %03i", paddlePosition-paddleHeight/2), 10, 40, 20, GRAY);
+            DrawText(TextFormat("Ball's X position is: %03i", ball.x), 10, 90, 20, GRAY);
+            DrawText(TextFormat("Ball's Y position is: %03i", ball.y), 10, 140, 20, GRAY);
+            DrawText(TextFormat("paddlePosition+paddleHeight is: %03i", (paddlePosition+paddleHeight)), 10, 190, 20, GRAY);
+            DrawText(TextFormat("SCORE is: %03i", score), screenWidth/2, 80, 30, GRAY);
+            DrawText(TextFormat("BOUNCE is: %03i", bounce), screenWidth/2, 120, 30, GRAY);
+
+
 
 
         EndDrawing();
